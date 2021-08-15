c42964c697fd76208889422a2c8f718b208bcb10
Peter <jingfs@Hotmail.com>
2021-08-15 13:24:42 +0930

    GUNI-ALGORITHM26-W2: assignment & study notes

---
+++ week02/StudyNotes.md
@@ -0,0 +1,29 @@
+Study Notes:
+
+
+# HashMap
+- Key-value
+- Hash function
+- Hashcode in Java
+- Hash collisions
+
+# Linked List 
+## Java code
+### Map: Key-value, unique key
+- new HashMap()/ new TreeMap()
+- map.set(key, value);
+- map.get(key)
+- map.has(key)
+- map.size()
+- map.clear()
+
+### Set: unique value
+- new HashSet()/ new TreeSet()
+- set.add(value)
+- set.delete(value)
+- set.has(value)
+
+https://docs.oracle.com/en/java/javase/12/docs/api/java.base/java/util/Set.html
+
+https://docs.oracle.com/en/java/javase/12/docs/api/java.base/java/util/Map.html
+
---
+++ week02/twoSum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> visitedElements = new HashMap();
+        
+        for (int i = 0; i < nums.length; i++) {
+            visitedElements.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (visitedElements.containsKey(difference) && visitedElements.get(difference) != i) {
+                return new int[] {i, visitedElements.get(difference)};
+            }
+        }
+        
+        return null;
+    }
+}
